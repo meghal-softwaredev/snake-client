390e2009da67f5277505cab607d3448b4c6db62e
Meghal <meghal.shah210@gmail.com>
2021-09-22 14:44:05 -0400

    Stdin setup

--- play.js
+++ play.js
@@ -1,19 +1,20 @@
 const net = require("net");
+const { exit } = require("process");
 const {connect} = require('./client');
-console.log(connect);
 
 console.log("Connecting ...");
 let conn = connect();
-conn.write('Name: MAP');
-setTimeout(() => {
-  conn.write('Move: up');
-}, 1000);
-setTimeout(() => {
-  conn.write('Move: left');
-},2000);
-setTimeout(() => {
-  conn.write('Move: down');
-},3000);
-setTimeout(() => {
-  conn.write('Move: right');
-},4000);
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+};
+setupInput();
