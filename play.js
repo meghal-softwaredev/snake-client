a38886a319428ea7f5e9c4adda670990a6497020
Meghal <meghal.shah210@gmail.com>
2021-09-22 12:55:21 -0400

    Connection module

--- play.js
+++ play.js
@@ -2,12 +2,13 @@ const net = require("net");
 
 // establishes a connection with the game server
 const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.223.133",
-    port: 50542,
+const {connect} = require('./client');
+const conn = net.createConnection({
+  host: "135.23.223.133",
+  port: 50542,
   });
   conn.on("connect", () => {
-    console.log('Hello from Meghal');
+  console.log('Hello from Meghal'); 
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
