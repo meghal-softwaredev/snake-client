94fae0d23e43bf7e51bce27e0b0e567f82fe7c94
Meghal <meghal.shah210@gmail.com>
2021-09-22 13:13:45 -0400

    Snake Initial

--- play.js
+++ play.js
@@ -1,20 +1,10 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
 const {connect} = require('./client');
-const conn = net.createConnection({
-  host: "135.23.223.133",
-  port: 50542,
-  });
-  conn.on("connect", () => {
-  console.log('Hello from Meghal'); 
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
 
-  return conn;
-};
+// connect.on('data', (data) => {
+
+// });
+
 
 console.log("Connecting ...");
 connect();
