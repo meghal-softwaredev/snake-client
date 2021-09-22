24e7d3824e0d646cfec745d368723836ad6c724f
Meghal <meghal.shah210@gmail.com>
2021-09-22 13:50:14 -0400

    Move Commands

--- play.js
+++ play.js
@@ -1,10 +1,19 @@
 const net = require("net");
 const {connect} = require('./client');
-
-// connect.on('data', (data) => {
-
-// });
-
+console.log(connect);
 
 console.log("Connecting ...");
-connect();
+let conn = connect();
+conn.write('Name: MAP');
+setTimeout(() => {
+  conn.write('Move: up');
+}, 1000);
+setTimeout(() => {
+  conn.write('Move: left');
+},2000);
+setTimeout(() => {
+  conn.write('Move: down');
+},3000);
+setTimeout(() => {
+  conn.write('Move: right');
+},4000);
