c2b19f95ddeeb4dfc557b63af4175e2bf2cdf19a
Meghal <meghal.shah210@gmail.com>
2021-09-22 15:11:52 -0400

    Sending messages

--- input.js
+++ input.js
@@ -23,6 +23,9 @@ const setupInput = function (conn) {
     if (key === 'd') {
       conn.write('Move: right');
     }
+    if (key === 'm') {
+      conn.write('Say: Hi!');
+    }
   });
   return stdin;
 };
